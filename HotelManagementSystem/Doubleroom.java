856c98c6dd7e0b4b8c8870de853f9b36a8043cee
Minn Thant <95152361+Thant998@users.noreply.github.com>
2022-06-19 21:00:16 +0630

    Add files via upload

---
+++ HotelManagementSystem/Doubleroom.java
@@ -0,0 +1,21 @@
+class Doubleroom extends Singleroom implements Serializable
+{
+    String name2;
+    String contact2;
+    String gender2;
+
+    Doubleroom()
+    {
+        this.name="";
+        this.name2="";
+    }
+    Doubleroom(String name,String contact,String gender,String name2,String contact2,String gender2)
+    {
+        this.name=name;
+        this.contact=contact;
+        this.gender=gender;
+        this.name2=name2;
+        this.contact2=contact2;
+        this.gender2=gender2;
+    }
+}
