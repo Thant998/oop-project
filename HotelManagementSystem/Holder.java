569c051a962fd328de6571373b479dda9aeb3617
Minn Thant <95152361+Thant998@users.noreply.github.com>
2022-06-19 21:01:27 +0630

    Add files via upload

---
+++ HotelManagementSystem/Holder.java
@@ -0,0 +1,8 @@
+
+class Holder implements Serializable
+{
+    Doubleroom luxury_doublerrom[]=new Doubleroom[10]; //Luxury
+    Doubleroom deluxe_doublerrom[]=new Doubleroom[20]; //Deluxe
+    Singleroom luxury_singleerrom[]=new Singleroom[10]; //Luxury
+    Singleroom deluxe_singleerrom[]=new Singleroom[20]; //Deluxe
+}
