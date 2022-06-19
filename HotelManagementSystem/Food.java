985f9bdf4bf9e367941ba9523aa4c5822a567d2e
Minn Thant <95152361+Thant998@users.noreply.github.com>
2022-06-19 20:59:21 +0630

    Add files via upload

---
+++ HotelManagementSystem/Food.java
@@ -0,0 +1,34 @@
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Food implements Serializable
+{
+    int itemno;
+    int quantity;
+    float price;
+
+    Food(int itemno,int quantity)
+    {
+        this.itemno=itemno;
+        this.quantity=quantity;
+        switch(itemno)
+        {
+            case 1:price=quantity*50;
+                break;
+            case 2:price=quantity*60;
+                break;
+            case 3:price=quantity*70;
+                break;
+            case 4:price=quantity*30;
+                break;
+        }
+    }
+}
